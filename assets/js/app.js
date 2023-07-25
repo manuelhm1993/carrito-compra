7478157e372e101482737dc6e0d8864e1232bc50
Manuel Henriquez <manuelhm1993@gmail.com>
2023-07-25 04:47:04 -0400

    Pintar el nuevo item con objetos
    Se crea el nuevo item y se agrega a la lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,29 @@
-const contenedorPrincipal = document.querySelector('main.container');
+const contenedorMain = document.querySelector('main.container');
 const carrito = document.querySelector('#carrito');
-const templateCarrito = document.querySelector('#template-item-carrito').content;
+const templateItemCarrito = document.querySelector('#template-item-carrito').content;
 const fragment = document.createDocumentFragment();
 
-const agregarItem = (elemento) => {
-    if(elemento.classList.value === 'btn btn-outline-primary') {
-        const clonCarrito = templateCarrito.cloneNode(true);
+const crearItem = (item) => {
+    const itemsCompra = carrito.querySelectorAll('li');
 
-        clonCarrito.querySelector('span.lead').textContent = elemento.dataset.item;
-        clonCarrito.querySelector('span.badge').textContent = 1;
+    nuevoItem = {
+        id: item,
+        descripcion: item,
+        cantidad: 1
+    };
 
-        fragment.appendChild(clonCarrito);
-        carrito.appendChild(fragment);
-    }
+    carrito.textContent = '';
+    let clonTemplateItemCarrito = templateItemCarrito.cloneNode(true);
+
+    clonTemplateItemCarrito.querySelector('li span.lead').textContent = nuevoItem.descripcion;
+    clonTemplateItemCarrito.querySelector('li span.badge').textContent = nuevoItem.cantidad;
+    fragment.appendChild(clonTemplateItemCarrito);
+
+    carrito.appendChild(fragment);
 };
 
-const incrementarContador = (listaItems, elemento) => {
-    let incremento = false;
-
-    for (let item of listaItems) {
-        incremento = item.querySelector('span.lead').textContent == elemento.dataset.item;
-
-        if(incremento) {
-            item.querySelector('span.badge').textContent = parseInt(item.querySelector('span.badge').textContent, 10) + 1;
-            break;
-        }
+contenedorMain.addEventListener('click', (e) => {
+    if(e.target.classList.value === 'btn btn-outline-primary') {
+        crearItem(e.target.dataset.item);
     }
-
-    return incremento;
-};
-
-contenedorPrincipal.addEventListener('click', (e) => {
-    const elemento = e.target;
-    const listaItems = carrito.querySelectorAll('li');
-
-    if((listaItems.length === 0) || (!incrementarContador(listaItems, elemento))) agregarItem(elemento);
 });
