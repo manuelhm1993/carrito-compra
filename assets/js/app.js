febc906ff44650fa30e22c63d99d4bfcbad19465
Manuel Henriquez <manuelhm1993@gmail.com>
2023-07-25 05:41:35 -0400

    Carrito terminado con objetos
    Se usaron objetos y sus propiedades para realizar la simulacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,55 @@
+// -------------------- Declaracion de variables
 const contenedorMain = document.querySelector('main.container');
 const carrito = document.querySelector('#carrito');
 const templateItemCarrito = document.querySelector('#template-item-carrito').content;
 const fragment = document.createDocumentFragment();
 
+// -------------------- Crear un objeto global que almacena la compra
+const carritoObjeto = {};
+
+// -------------------- Declaracion de funciones
 const crearItem = (item) => {
+    // -------------------- Traer la compra
     const itemsCompra = carrito.querySelectorAll('li');
 
+    // -------------------- Crear el nuevo item
     nuevoItem = {
         id: item,
         descripcion: item,
         cantidad: 1
     };
 
-    carrito.textContent = '';
-    let clonTemplateItemCarrito = templateItemCarrito.cloneNode(true);
+    // -------------------- Incrementar la cantidad del item si ya se encuentra agregado
+    if(carritoObjeto.hasOwnProperty(nuevoItem.id)) {
+        carritoObjeto[nuevoItem.id].cantidad++;
+    }
+    else {
+        // -------------------- Agregar el nuevo item al carrito
+        carritoObjeto[nuevoItem.descripcion] = nuevoItem;
+    }
 
-    clonTemplateItemCarrito.querySelector('li span.lead').textContent = nuevoItem.descripcion;
-    clonTemplateItemCarrito.querySelector('li span.badge').textContent = nuevoItem.cantidad;
-    fragment.appendChild(clonTemplateItemCarrito);
+    // -------------------- Formatear el carrito
+    carrito.textContent = '';
+
+    pintarCarrito();
+};
+
+const pintarCarrito = () => {
+    // -------------------- Obtener un array con los valores del objeto
+    Object.values(carritoObjeto).forEach(item => {
+        // -------------------- Crear los objetos de la plantilla
+        const clonTemplateItemCarrito = templateItemCarrito.cloneNode(true);
+
+        clonTemplateItemCarrito.querySelector('li span.lead').textContent = item.descripcion;
+        clonTemplateItemCarrito.querySelector('li span.badge').textContent = item.cantidad;
+
+        fragment.appendChild(clonTemplateItemCarrito);
+    });
 
     carrito.appendChild(fragment);
 };
 
+// -------------------- Delegacion de eventos para los botones del contenedor
 contenedorMain.addEventListener('click', (e) => {
     if(e.target.classList.value === 'btn btn-outline-primary') {
         crearItem(e.target.dataset.item);
