ec568f6edc7ce9d571197007cb090a1c8cd609cc
Manuel Henriquez <manuelhm1993@gmail.com>
2023-07-27 12:31:10 -0400

    Se crearon las variables objetos del footer para manipular el template de total, se modificó el objeto nuevoItem para agregar la propiedad precio y se quitó la propiedad firstElementChild antes de clonar el template del carrito ya que ahora tiene dos nodos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,15 @@
 // -------------------- Declaracion de variables
 const contenedorMain = document.querySelector('main.container');
+
+// -------------------- Template carrito
 const carrito = document.querySelector('#carrito');
 const templateItemCarrito = document.querySelector('#template-item-carrito').content;
-const fragment = document.createDocumentFragment();
+const fragmentCarrito = document.createDocumentFragment();
+
+// -------------------- Template total compra
+const totalCompra = document.querySelector('#total-compra');
+const templateTotalCompra = document.querySelector('#template-total-compra').content;
+const fragmentTotalCompra = document.createDocumentFragment();
 
 // -------------------- Crear un array global que almacena la compra
 const carritoArray = [];
@@ -11,9 +18,10 @@ const carritoArray = [];
 const crearItem = (item) => {
     // -------------------- Crear el nuevo item
     const nuevoItem = {
-        id: item,
-        descripcion: item,
-        cantidad: 1
+        id: item.descripcion,
+        descripcion: item.descripcion,
+        cantidad: 1,
+        precio: item.precio
     };
 
     guardarCarrito(nuevoItem);
@@ -34,21 +42,35 @@ const leerCarrito = (arrayCompra) => {
     
     // -------------------- Iterar el array y pintar los elementos en el DOM usando el template
     arrayCompra.forEach(item => {
-        // -------------------- Crear los objetos de la plantilla
-        const clonTemplateItemCarrito = templateItemCarrito.firstElementChild.cloneNode(true);
+        // -------------------- Como son varios nodos, no se puede usar ni firstElementChild ni lastElementChild
+        const clonTemplateItemCarrito = templateItemCarrito.cloneNode(true);
 
         clonTemplateItemCarrito.querySelector('li span.lead').textContent = item.descripcion;
         clonTemplateItemCarrito.querySelector('li span.badge').textContent = item.cantidad;
+        clonTemplateItemCarrito.querySelector('li div p.lead span').textContent = item.precio;
 
-        fragment.appendChild(clonTemplateItemCarrito);
+        fragmentCarrito.appendChild(clonTemplateItemCarrito);
     });
 
-    carrito.appendChild(fragment);
+    carrito.appendChild(fragmentCarrito);
 };
 
 // -------------------- Delegacion de eventos para los botones del contenedor
 contenedorMain.addEventListener('click', (e) => {
+    const fuenteEvento = e.target;
+
+    const item = {
+        descripcion: fuenteEvento.dataset.item,
+        precio: fuenteEvento.dataset.precio
+    };
+
+    if(fuenteEvento.classList.value === 'btn btn-outline-primary') {
+        crearItem(item);
+    }
+});
+
+totalCompra.addEventListener('click', (e) => {
     if(e.target.classList.value === 'btn btn-outline-primary') {
-        crearItem(e.target.dataset.item);
+        console.log('Compra finalizada')
     }
 });
