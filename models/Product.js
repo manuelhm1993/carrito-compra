ef53d6890306ff719d6d5beee9962c5b80825f4e
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-12 14:14:36 -0400

    Modelo de productos creado

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: 1,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'café',
+        price: 500
+    },
+    {
+        id: 2,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'pizza',
+        price: 300
+    },
+    {
+        id: 3,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'agua',
+        price: 100
+    },
+    {
+        id: 4,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'sandía',
+        price: 50
+    },
+    {
+        id: 5,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'mango',
+        price: 10
+    },
+    {
+        id: 6,
+        image: 'https://okdiario.com/img/2021/10/01/intensidad-cafe-655x368.jpg',
+        name: 'chela',
+        price: 150
+    }
+];
