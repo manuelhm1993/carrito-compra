10e5f9b4cade74328384c8305b2d31f7f9287d8f
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-12 18:46:04 -0400

    Algoritmo para agregar productos al carrito, validar si es un nuevo producto, si es existente incrementar su cantidad y calcular el total

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+// --------------- Variables
+const carritoCompra = [];
+let productosAPI;
+
 // --------------- Funciones
 // 
 // --------------- Obtener los productos del catálogo
@@ -7,6 +11,8 @@ const fetchProducts = async (url) => {
         const data = await res.json();
 
         console.table(data);
+
+        productosAPI = data;
 
         renderizarCatalogo(data);
     } catch (error) {
@@ -43,6 +49,31 @@ const renderizarCatalogo = (productos) => {
 // --------------- Colocar la primera letra en mayúscula
 const capitalize = (palabra) => (palabra.charAt(0).toUpperCase() + palabra.slice(1));
 
+// --------------- Agrega un nuevo item al carrito
+const agregarItem = (id) => {
+    // --------------- Busca el producto en la BBDD, pero se debe parsear ya que el dataset es un string
+    const itemSolicitado = productosAPI.find((producto) => producto.id === parseFloat(id));
+
+    if(itemSolicitado) {
+        // --------------- Si el producto está disponible, se busca en el carrito si está agregaado
+        const item = carritoCompra.find((item) => item.id === itemSolicitado.id);
+
+        // --------------- Si el producto solicitado no está en el carrito se agrega
+        if(!item) {
+            itemSolicitado.cantidad = 1;
+            itemSolicitado.total = itemSolicitado.price;
+            carritoCompra.push(itemSolicitado);
+        }
+        // --------------- Si el producto solicitado está en el carrito se incrementa la cantidad
+        else {
+            item.cantidad++;
+            item.total = item.cantidad * item.price;
+        }
+    }
+
+    console.log(carritoCompra);
+};
+
 // --------------- Delegación de eventos
 // 
 // --------------- Al cargar el documento
@@ -59,6 +90,6 @@ document.addEventListener('click', (e) => {
 
     // --------------- Comrpobar si el elemento el atributo data
     if(fuenteEvento.hasAttribute('data-producto-id')) {
-        console.log(fuenteEvento.dataset.productoId);
+        agregarItem(fuenteEvento.dataset.productoId);
     }
 });
