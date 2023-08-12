576e74588c7eaaf505702564f42de3b3c6a7a1e0
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-12 16:03:15 -0400

    Se completó el maquetado de y se hizo la conexión al archivo JSON simulando una API externa, tomar en cuenta que la url relativa se toma a partir del index.html y no del app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-console.log('Conectado');
+// --------------- Funciones
+// 
+// --------------- Obtener los productos del catálogo
+const fetchProducts = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        console.log(res);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    } finally {
+
+    }
+};
+
+// --------------- Delegación de eventos
+// 
+// --------------- Al cargar el documento
+document.addEventListener('DOMContentLoaded', (e) => {
+    const url = './API/products.json';
+
+    fetchProducts(url);
+});
